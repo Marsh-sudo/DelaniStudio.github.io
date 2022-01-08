b5f0ea5f1a3484c6d5d44fb86a3ee83e1104995b
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-01-08 16:54:22 +0300

    Add toggle method

--- js/script.js
+++ js/script.js
@@ -3,18 +3,47 @@ $(document).ready(function() {
         $("#p1").toggle();
         
     });
+});
+$(document).ready(function(){
     $("#dev").click(function(){
         $("#p2").toggle();
     });
+});
+$(document).ready(function(){
     $("#pro").click(function(){
         $("#p3").toggle();
     })
 });
+   
 
 function myFunction(e) {
     alert("We have Received Your Message! And Thank you for reaching out to us!");
     (e).preventDefault;
   }
+
+
+$(document).ready(function() {
+    $('.pics').mouseover(function(){
+        $(this).css('opacity','.2');
+        
+        
+    }).mouseout(function(){
+        $(this).css('opacity','1');
+       
+    });
+
+})
+
+
+$('.sed').click(function(){
+    $('#des').toggle();
+})
+$('.ved').click(function(){
+    $('#dev').toggle();
+ })
+ $('.man').click(function(){
+     $('#pro').toggle();
+ })
 
   
 
